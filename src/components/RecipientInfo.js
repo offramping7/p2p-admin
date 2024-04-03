6064c20bccb777c8c47051f06fa515d0a454ddf9
offramping7 <offramping7@mail.ru>
2024-04-03 07:05:57 +0500

    update schema to reflect latest changes

--- src/components/RecipientInfo.js
+++ src/components/RecipientInfo.js
@@ -9,11 +9,7 @@ import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 import { SERVER_URL } from "../utils/constants";
-import {
-    Link
-  } from "react-router-dom"
-
-
+import { Link } from "react-router-dom";
 
 function RecipientInfo({ address }) {
   const [recipientData, setRecipientData] = useState({
@@ -21,132 +17,140 @@ function RecipientInfo({ address }) {
     bankName: "",
     phoneNumber: "",
     email: "",
-    bankSpecificFieldsMap: {cardNumber:"",yooMoneyWalletNumber:""},
+    bankSpecificFieldValue:"",
+    cardPhoneNumber:""
   });
-  const [loading,setLoading] = useState(true)
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     fetchRecipientData();
   }, []);
 
   const fetchRecipientData = async () => {
-   
     const url = `${SERVER_URL}/recipients/address/${address}`;
-    return axios.get(url).then((res)=>{
-      const data = res.data
-      setRecipientData(data)
-      setLoading(false)
-    })
+    return axios.get(url).then((res) => {
+      const {nickname,bankName,phoneNumber,cardPhoneNumber,email,bankSpecificFieldValue} = res.data;
+      setRecipientData({nickname,bankName,phoneNumber,cardPhoneNumber,email,bankSpecificFieldValue});
+      setLoading(false);
+    });
   };
   if (loading === true) {
-    return (<>Loading, please wait</>)
+    return <>Loading, please wait</>;
   }
 
   return (
     <>
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 500 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Column</TableCell>
-            <TableCell align="right">Value</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-         
-         
-          <TableRow
-            key={"nickname"}
-            sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-          >
-            <TableCell component="th" scope="row">
-              {"Name"}
-            </TableCell>
-            <TableCell align="right">{recipientData?.nickname}</TableCell>
-          </TableRow>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 500 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Column</TableCell>
+              <TableCell align="right">Value</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            <TableRow
+              key={"nickname"}
+              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {"Name"}
+              </TableCell>
+              <TableCell align="right">{recipientData?.nickname}</TableCell>
+            </TableRow>
 
-          <TableRow
-            key={"bankName"}
-            sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-          >
-            <TableCell component="th" scope="row">
-              {"bankName"}
-            </TableCell>
-            <TableCell align="right">{recipientData?.bankName}</TableCell>
-          </TableRow>
-          <TableRow
-            key={"phoneNumber"}
-            sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-          >
-            <TableCell component="th" scope="row">
-              {"Phone: "}
-            </TableCell>
-            <TableCell align="right">{recipientData?.phoneNumber}</TableCell>
-          </TableRow>
-          
-          <TableRow
-            key={"email"}
-            sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-          >
-            <TableCell component="th" scope="row">
-              {"email"}
-            </TableCell>
-            <TableCell align="right">{recipientData?.email}</TableCell>
-          </TableRow>
-          <TableRow
-            key={"cardNumber"}
-            sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-          >
-            <TableCell component="th" scope="row">
-              {"Card Number"}
-            </TableCell>
-            <TableCell align="right">
-              {recipientData?.bankSpecificFieldsMap.cardNumber || "empty"}
-            </TableCell>
-          </TableRow>
-          <TableRow
-            key={"yooMoneyWalletNumber"}
-            sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-          >
-            <TableCell component="th" scope="row">
-              {"Yoomoney wallet number"}
-            </TableCell>
-            <TableCell align="right">
-            {recipientData?.bankSpecificFieldsMap.yooMoneyWalletNumber || "empty"}
-            </TableCell>
-          </TableRow>
-         
-        </TableBody>
-      </Table>
-    </TableContainer>
-    <div className="mt-5 w-50 mx-auto">
-    {
-        !!recipientData?.bankSpecificFieldsMap?.cardNumber && (
-                <Link  to={{ pathname: "https://bincheck.io/details/"+recipientData?.bankSpecificFieldsMap?.cardNumber.slice(0,8) }} target="_blank" >
-                        Lookup Bin
-                    </Link>
-        )
-    }
-    </div>
+            <TableRow
+              key={"bankName"}
+              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {"bankName"}
+              </TableCell>
+              <TableCell align="right">{recipientData?.bankName}</TableCell>
+            </TableRow>
+            <TableRow
+              key={"phoneNumber"}
+              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {"Phone: "}
+              </TableCell>
+              <TableCell align="right">{recipientData?.phoneNumber}</TableCell>
+            </TableRow>
 
-    <div className="mt-5 w-50 mx-auto">
-    {
-        !!recipientData?.bankSpecificFieldsMap?.cardNumber && (
-                <a  href={"https://bincheck.io/details/"+recipientData?.bankSpecificFieldsMap?.cardNumber.slice(0,8) } >
-                        Lookup Bin
-                    </a>
-        )
-    }
-    </div>
-   
-    
+            <TableRow
+              key={"email"}
+              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {"email"}
+              </TableCell>
+              <TableCell align="right">{recipientData?.email}</TableCell>
+            </TableRow>
+            <TableRow
+              key={"cardNumber"}
+              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {"Card Number"}
+              </TableCell>
+              <TableCell align="right">
+                {recipientData?.bankSpecificFieldsMap.cardNumber || "empty"}
+              </TableCell>
+            </TableRow>
+            <TableRow
+              key={"yooMoneyWalletNumber"}
+              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {"Yoomoney wallet number"}
+              </TableCell>
+              <TableCell align="right">
+                {recipientData?.bankSpecificFieldsMap.yooMoneyWalletNumber ||
+                  "empty"}
+              </TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <div className="mt-5 w-50 mx-auto">
+        {!!recipientData?.bankSpecificFieldsMap?.cardNumber && (
+          <Link
+            to={{
+              pathname:
+                "https://bincheck.io/details/" +
+                recipientData?.bankSpecificFieldsMap?.cardNumber.slice(0, 8),
+            }}
+            target="_blank"
+          >
+            Lookup Bin
+          </Link>
+        )}
+      </div>
 
-<div className="mt-5 w-50 mx-auto">
-{!!recipientData?.phoneNumber && (<Link to={{ pathname: "https://wa.me/"+recipientData.phoneNumber  }} target="_blank">
-        Message on whatsapp
-    </Link>)}
-</div>
-    
+      <div className="mt-5 w-50 mx-auto">
+        {!!recipientData?.bankSpecificFieldsMap?.cardNumber && (
+          <a
+            href={
+              "https://bincheck.io/details/" +
+              recipientData?.bankSpecificFieldsMap?.cardNumber.slice(0, 8)
+            }
+          >
+            Lookup Bin
+          </a>
+        )}
+      </div>
+
+      <div className="mt-5 w-50 mx-auto">
+        {!!recipientData?.phoneNumber && (
+          <Link
+            to={{ pathname: "https://wa.me/" + recipientData.phoneNumber }}
+            target="_blank"
+          >
+            Message on whatsapp
+          </Link>
+        )}
+      </div>
     </>
   );
 }
